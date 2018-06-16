bee346cccb363c37f34b3167aa9535ee2d5e623a
Zoldur <zoldur99@gmail.com>
2018-06-16 03:17:05 -0700

    Get IP

--- fornix_install.sh
+++ fornix_install.sh
@@ -11,7 +11,7 @@ COIN_PORT=24246
 RPCPORT=25256
 
 
-NODEIP=$(curl -s4 icanhazip.com)
+NODEIP=$(curl -s4 api.ipify.org)
 
 
 RED='\033[0;31m'
@@ -138,7 +138,7 @@ function get_ip() {
   declare -a NODE_IPS
   for ips in $(netstat -i | awk '!/Kernel|Iface|lo/ {print $1," "}')
   do
-    NODE_IPS+=($(curl --interface $ips --connect-timeout 2 -s4 icanhazip.com))
+    NODE_IPS+=($(curl --interface $ips --connect-timeout 2 -s4 api.ipify.org))
   done
 
   if [ ${#NODE_IPS[@]} -gt 1 ]
